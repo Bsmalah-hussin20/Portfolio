913dc4871adbb6b6af877dec928634358a1f264d
Bsmalah-hussin20 <bsmalahhussin@gmail.com>
2025-02-26 01:10:35 +0200

    Add 2 sections to main

---
+++ script.js
@@ -0,0 +1,20 @@
+var swiper = new Swiper('.serviceSwiper',{
+    slidesPerView:3,
+    spaceBetween:30,
+    loop:true,
+    autoplay:true,
+    // breakpoints:{
+    //     1200:{
+    //         slidesPerView:3,
+    //         spaceBetween:30,
+    //     },
+    //     900:{
+    //         slidesPerView:2,
+    //         spaceBetween:30,
+    //     },
+    //     500:{
+    //         slidesPerView:3,
+    //         spaceBetween:30,
+    //     }
+    // }
+});
