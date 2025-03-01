457a00e1a731c39874b382f64463d3dff39ccd1d
Bsmalah-hussin20 <bsmalahhussin@gmail.com>
2025-03-01 17:51:25 +0200

    Add new section to main

--- script.js
+++ script.js
@@ -3,18 +3,39 @@ var swiper = new Swiper('.serviceSwiper',{
     spaceBetween:30,
     loop:true,
     autoplay:true,
-    // breakpoints:{
-    //     1200:{
-    //         slidesPerView:3,
-    //         spaceBetween:30,
-    //     },
-    //     900:{
-    //         slidesPerView:2,
-    //         spaceBetween:30,
-    //     },
-    //     500:{
-    //         slidesPerView:3,
-    //         spaceBetween:30,
-    //     }
-    // }
+    breakpoints:{
+        1200:{
+            slidesPerView:3,
+            spaceBetween:30,
+        },
+        900:{
+            slidesPerView:2,
+            spaceBetween:30,
+        },
+        500:{
+            slidesPerView:3,
+            spaceBetween:30,
+        }
+    }
+});
+
+var swiper = new Swiper('.testimomialSwiper',{
+    slidesPerView:2,
+    spaceBetween:10,
+    loop:true,
+    autoplay:true,
+    breakpoints:{
+        1200:{
+            slidesPerView:2,
+            spaceBetween:10,
+        },
+        900:{
+            slidesPerView:2,
+            spaceBetween:10,
+        },
+        500:{
+            slidesPerView:1,
+            spaceBetween:10,
+        }
+    }
 });
